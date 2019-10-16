f6f5bac150d54dd81d10fa8e67dd2e2d359189d4
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-10-16 12:23:44 +0100

    Finished Contract 5

---
+++ Contract 5/calibrate.py
@@ -0,0 +1,54 @@
+#15 colour, 4 white
+from PIL import Image
+import colorsys
+
+img = Image.open("calibration.png").convert("RGB")
+imgData = img.load()
+Normal = []
+Deuteranomaly = []
+Protanomaly = []
+Protanopia = []
+Deuteranopia = []
+Tritanopia = []
+Tritanomaly = []
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 0]]
+    Normal.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 21]]
+    Deuteranomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 42]]
+    Protanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 63]]
+    Protanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 103]]
+    Deuteranopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 21]]
+    Tritanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+for x in range(img.size[1]):
+    colour = [x/255 for x in imgData[x, 118]]
+    Tritanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+
+with open("blindnessValues.txt", 'w') as file:
+    file.write(','.join([str(x) for x in Deuteranomaly]) + "\n")
+    file.write(','.join([str(x) for x in Protanomaly]) + "\n")
+    file.write(','.join([str(x) for x in Protanopia]) + "\n")
+    file.write(','.join([str(x) for x in Deuteranopia]) + "\n")
+    file.write(','.join([str(x) for x in Tritanopia]) + "\n")
+    file.write(','.join([str(x) for x in Tritanomaly]) + "\n")
+
+
+
+
+
+
---
+++ Contract 5/convert.py
@@ -0,0 +1,29 @@
+import colorsys
+from PIL import Image
+
+
+def normal_to_colour_blind(_normal_colour: (int, int, int), conversion_list: list) -> (int, int, int):
+    normal_colour = [x/255 for x in _normal_colour]
+    normal_hsv = colorsys.rgb_to_hsv(normal_colour[0], normal_colour[1], normal_colour[2])
+    blind_index = int(round(normal_hsv[0] * 150))
+    blind_hue = conversion_list[blind_index]
+    blind_colour = colorsys.hsv_to_rgb(blind_hue/255, normal_hsv[1], normal_hsv[2])
+    return tuple([int(round(x*255)) for x in blind_colour])
+
+
+mode = 0 # Deuteranomaly Protanomaly Deuteranopia Tritanopia Tritanomaly
+
+values = []
+with open("blindnessValues.txt", 'r') as file:
+    for i in range(mode + 1):
+        valuesstr = file.readline()
+
+values = [int(x) for x in valuesstr.split(',')]
+
+normal = Image.open("test.jpg").convert("RGB")
+normal_data = normal.load()
+for x in range(normal.size[0]):
+    for y in range(normal.size[1]):
+        normal_data[x,y] = normal_to_colour_blind(normal_data[x,y], values)
+
+normal.show()
