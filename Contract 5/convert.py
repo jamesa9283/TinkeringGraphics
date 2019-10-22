6963bce52d4f9a12e1bf495662aabee7d72a0e70
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-10-22 09:12:48 +0100

    Improved PEP8 in convert.py

--- Contract 5/convert.py
+++ Contract 5/convert.py
@@ -4,12 +4,14 @@ from PIL import Image
 
 
 def normal_to_colour_blind(_normal_colour: (int, int, int), conversion_list: list) -> (int, int, int):
-    normal_colour = [x/255 for x in _normal_colour]
+    normal_colour = [x / 255 for x in _normal_colour]
     normal_hsv = colorsys.rgb_to_hsv(normal_colour[0], normal_colour[1], normal_colour[2])
+    
     blind_index = int(round(normal_hsv[0] * 173))
     blind_hue = conversion_list[blind_index]
-    blind_colour = colorsys.hsv_to_rgb(blind_hue/255, normal_hsv[1], normal_hsv[2])
-    return tuple([int(round(x*255)) for x in blind_colour])
+    blind_colour = colorsys.hsv_to_rgb(blind_hue / 255, normal_hsv[1], normal_hsv[2])
+
+    return tuple([int(round(x * 255)) for x in blind_colour])
 
 
 for i in range(6):
@@ -23,8 +25,8 @@ for i in range(6):
     normal = Image.open("test.jpg").convert("RGB")
     normal_data = normal.load()
     for x in range(normal.size[0]):
-       for y in range(normal.size[1]):
-            normal_data[x,y] = normal_to_colour_blind(normal_data[x,y], values)
+        for y in range(normal.size[1]):
+            normal_data[x, y] = normal_to_colour_blind(normal_data[x, y], values)
 
     normal.show()
     normal.save("converted"+str(i)+".png")
