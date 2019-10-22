dcaaf8e5077fb23a8e0be47fcab65f2b9a55d072
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-10-22 08:53:44 +0100

    Improved PEP8 compatibility in calibrate.py

--- Contract 5/calibrate.py
+++ Contract 5/calibrate.py
@@ -1,51 +1,50 @@
-#15 colour, 4 white
 from PIL import Image
 import colorsys
 
 img = Image.open("calibration.png").convert("RGB")
 imgData = img.load()
 Normal = []
-Deuteranomaly = []
-Protanomaly = []
-Protanopia = []
-Deuteranopia = []
-Tritanopia = []
-Tritanomaly = []
+deuteranomaly = []
+protanomaly = []
+protanopia = []
+deuteranopia = []
+tritanopia = []
+tritanomaly = []
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 0]]
     Normal.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 21]]
-    Deuteranomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    deuteranomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 42]]
-    Protanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    protanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 63]]
-    Protanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    protanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 84]]
-    Deuteranopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    deuteranopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 103]]
-    Tritanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    tritanopia.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 for x in range(img.size[0]):
     colour = [x/255 for x in imgData[x, 118]]
-    Tritanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0]*255)))
+    tritanomaly.append(int(round(colorsys.rgb_to_hsv(colour[0], colour[1], colour[2])[0] * 255)))
 
 with open("blindnessValues.txt", 'w') as file:
-    file.write(','.join([str(x) for x in Deuteranomaly]) + "\n")
-    file.write(','.join([str(x) for x in Protanomaly]) + "\n")
-    file.write(','.join([str(x) for x in Protanopia]) + "\n")
-    file.write(','.join([str(x) for x in Deuteranopia]) + "\n")
-    file.write(','.join([str(x) for x in Tritanopia]) + "\n")
-    file.write(','.join([str(x) for x in Tritanomaly]) + "\n")
+    file.write(','.join([str(x) for x in deuteranomaly]) + "\n")
+    file.write(','.join([str(x) for x in protanomaly]) + "\n")
+    file.write(','.join([str(x) for x in protanopia]) + "\n")
+    file.write(','.join([str(x) for x in deuteranopia]) + "\n")
+    file.write(','.join([str(x) for x in tritanopia]) + "\n")
+    file.write(','.join([str(x) for x in tritanomaly]) + "\n")
 
 
 
